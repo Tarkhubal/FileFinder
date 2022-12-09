79535498a9a36baa4ba17fc299fe5cc8f97475d8
The_WildThomas <thb5309@gmail.com>
2022-12-09 17:33:34 +0100

    First Release

---
+++ README.md
@@ -0,0 +1,16 @@
+# File finder: README.md
+
+## What is this?
+
+For now, the script find files with a certain extension in the "input" directory and move them to the "output" directory.
+
+---
+
+## How to use?
+
+1. Install Python 3.7.3 or higher
+2. Install the requirements: `pip install -r requirements.txt`
+3. Move your heap of files you want to sort to the `input` directory
+4. Edit the file `ext-to-filter.txt` and add the extensions you want to find (one per line)
+5. Run the script: `python main.py`
+6. Collect your files in the `output` directory
---
+++ main.py
@@ -0,0 +1,38 @@
+import pathlib
+import shutil
+
+class ANSI():
+    def color(bg, fg, style):
+        return "\33[{bg};{fg};{style}m".format(bg=bg, fg=fg, style=style)
+
+
+all_ext = open("ext-to-filter.txt", "r").read().split("\n")
+if "" in all_ext:
+    all_ext.remove("")
+print("Filtering by : " + str(all_ext))
+
+nbfiles = 0
+file_nb = 1
+file_moved = 0
+
+for path in pathlib.Path("./input").iterdir():
+    if path.is_file():
+        nbfiles += 1
+print("There is " + str(nbfiles) + " files in the input folder.")
+
+for path in pathlib.Path("./input").iterdir():
+    file = pathlib.PurePosixPath(path).name
+    
+    for ext in all_ext:
+        if file.endswith(ext):
+            print(ANSI.color(7, 49, 32) + "--> File \"" + str(file) + "\" (file number " + str(file_nb) + ") is a     " + str(ext) + " file" + ANSI.color(0, 94, 39))
+            file_moved += 1
+            
+            shutil.move("./input/" + str(file), "./output/" + str(file))
+            break
+        else:
+            print("    File \"" + str(file) + "\" (file number " + str(file_nb) + ") is not a " + str(ext) + " file")
+    file_nb += 1
+
+print("Successfully moved " + str(file_moved) + " files out of " + str(nbfiles) + " files in the output folder !")
+exit = input("Press enter to exit: ")
